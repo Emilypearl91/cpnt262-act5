63a726e62e23b0c902d4a42dfab0312e7d5c1278
Emily Pearl <Emilypearl9193@gmail.com>
2023-11-09 08:58:11 -0700

    footer,nav and updated read me

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cpnt262-act5
+
+## Emily Seafoot
+[my github](https://github.com/Emilypearl91)
+
+### Attributions:
+
+
+[github repo](https://github.com/Emilypearl91/cpnt262-act5)
--- cpnt262-act5/src/routes/+page.svelte
+++ cpnt262-act5/src/routes/+page.svelte
@@ -1,2 +1,92 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+  <ul>
+     <li><a href="/">link</a></li>
+    <li><a href="/">link</a></li>
+    <li><a href="/">link</a></li>
+    <li><a href="/">link</a></li>
+  </ul>
+</nav>
+
+
+<main>
+  <footer>
+    <ul>
+      <li><a href="/">link</a></li>
+      <li><a href="/">link</a></li>
+      <li><a href="/">link</a></li>
+    </ul>
+    <p>© 2023 Emily Seafoot</p>
+ </footer>
+</main>
+
+<style>
+  nav{
+  height: 3rem;
+  background-color: black;
+  color: white;
+  margin-top: 0;
+  
+}
+
+nav ul{
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  margin: auto;
+
+  background-color: black;
+
+  padding: 0;
+  height: 3rem;
+  
+}
+
+nav li{
+  display: inline-block;
+  padding: 1rem;
+  
+}
+
+nav a{
+  text-decoration: none; 
+  color: white;
+
+}
+
+nav a:hover{
+  color: white;
+} 
+
+main {
+  display: flex;
+  flex-direction: column;
+  flex: auto;
+}
+
+footer {
+  padding-top: 2rem;
+  height: 3rem;
+  margin-top: 3rem;
+
+  background-color: black;
+}
+
+footer p {
+ margin: auto;
+ text-align: center;
+ padding-top: 3rem;
+}
+
+footer ul {
+  list-style: none;
+  display: flex;
+  justify-content: space-evenly;
+
+  margin: auto;
+}
+
+footer a {
+  text-decoration: none;
+  color: white;
+}
+</style>
